20f36696c9c8537c9667ff3fa7b53af92ca177b5
Alexander Levero <Alexander.Levero@gmail.com>
2023-06-28 23:47:47 +0300

    Added initial authorization docs and updated requirements and yml files. -Alex

---
+++ api/accounts.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel
+
+
+class DuplicateAccountError(ValueError):
+    pass
+
+
+class AccountIn(BaseModel):
+    email: str
+    password: str
+    full_name: str
+
+
+class AccountOut(BaseModel):
+    id: str
+    email: str
+    full_name: str
+
+
+class AccountOutWithPassword(AccountOut):
+    hashed_password: str
+
+class AccountQueries(Queries):
+    # region properties --
+
+    def get(self, email: str) -> AccountOut:
+
+    def create(self, info: AccountIn, hashed_password: str) -> AccountOut:
--- api/main.py
+++ api/main.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from authenticator import authenticator
 import os
 
 app = FastAPI()
+app.include_router(authenticator.router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        os.environ.get("CORS_HOST", "http://localhost:3000")
-    ],
+    allow_origins=[os.environ.get("CORS_HOST", "http://localhost:3000")],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -23,6 +23,6 @@ def launch_details():
             "week": 17,
             "day": 5,
             "hour": 19,
-            "min": "00"
+            "min": "00",
         }
     }
