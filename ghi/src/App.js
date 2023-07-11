38c9ae9d47b49b3f95c774425db626b73eb9fc9c
Alexander Levero <Alexander.Levero@gmail.com>
2023-07-11 16:11:15 -0400

    Added front-end garden pages, added front-end authentication dependencies. -Alex

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -7,26 +7,29 @@ import LogIn from "./accounts/LoginForm";
 import JournalLists from "./journals/JournalLists";
 import JournalDetails from "./journals/JournalDetails";
 import JournalForm from "./journals/JournalForm";
+import { AuthProvider } from "@galvanize-inc/jwtdown-for-react";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Nav />
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="accounts">
-            <Route path="signup" element={<SignUp />} />
-            <Route path="login" element={<LogIn />} />
-          </Route>
-          <Route path="journals">
-            <Route path="" element={<JournalLists />} />
-            <Route path=":journal_id" element={<JournalDetails />} />
-            <Route path="create" element={<JournalForm />} />
-          </Route>
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Nav />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="accounts">
+              <Route path="signup" element={<SignUp />} />
+              <Route path="login" element={<LogIn />} />
+            </Route>
+            <Route path="journals">
+              <Route path="" element={<JournalLists />} />
+              <Route path=":journal_id" element={<JournalDetails />} />
+              <Route path="create" element={<JournalForm />} />
+            </Route>
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
--- journals/alexander-levero.md
+++ journals/alexander-levero.md
@@ -1,7 +1,48 @@
-## 26 JUL 2023
+## 26 JUN 2023
 
 Today I worked on:
 
-* Creating a total of 15 issue tickets with Linda
+- Creating a total of 15 issue tickets with Linda
 
 We described the various functionalities we would like the App to contain, and brainstormed solutions for various design problems.
+
+## 27 JUN 2023
+
+Today I worked on:
+
+- Creating initial MongoDB setup
+- Created journal files
+
+I did a code-along with my team, doing all of the initial setup for MongoDB by setting up the yml and requirements files.
+
+## 28 JUN 2023
+
+Today I worked on:
+
+- Started work on authorization files
+
+I began to add authorization functionality code to our app.
+
+## 29 JUN 2023
+
+Today I worked on:
+
+- Completed and tested authorization
+
+I was able to successfully implement authorization in our app and fully tested it, and then showed the team how the functionality and code works.
+
+## 10 JUL 2023
+
+Today I worked on:
+
+- Began work on Garden list, detail, and delete pages
+
+## 11 JUL 2023
+
+Today I worked on:
+
+- Choosing color palette for front-end
+- Front end troubleshooting
+- Implementing front-end authentication
+
+Settled on a color palette with the team, beginning to implement it on the front-end. Helped teammates troubleshoot issues with front-end. Installed dependencies for front-end authentication with JWTdown.
