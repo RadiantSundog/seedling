d6688cdda6d5ab24125561bfa90fbc974a17e5ae
kyrstin jones <kyrstin.jonesgmail.com>
2023-07-18 08:41:54 -0400

    Just new backend code, old wasn't workign anyway. -Kyrstin

--- api/queries/plants.py
+++ api/queries/plants.py
@@ -1,8 +1,7 @@
 from pydantic import BaseModel, HttpUrl
-from typing import List, Dict, Any, Optional
+from typing import List, Optional
 from queries.database import db
 from bson import ObjectId
-from models import GardenOut
 
 
 class PlantIn(BaseModel):
@@ -26,92 +25,27 @@ class PlantRepository:
     plants_collection = db.plants
     gardens_collection = db.gardens
 
-    # def create(self, plant: PlantIn) -> PlantOut:
-    #     props = plant.dict()
-    #     self.plants_collection.insert_one(props)
-    #     props["id"] = str(props["_id"])
-    #     self.gardens_collection.update_one(
-    #         {"_id": ObjectId(plant.garden_id)},
-    #         {"$push": {"plant_ids": props["_id"]}},
-    #     )
-    #     return PlantOut(**props)
-
     def create(self, plant: PlantIn) -> PlantOut:
-        garden = self.gardens_collection.find_one(
-            {"_id": ObjectId(plant.garden_id)}
+        props = plant.dict()
+        self.plants_collection.insert_one(props)
+        props["id"] = str(props["_id"])
+        self.gardens_collection.update_one(
+            {"_id": ObjectId(plant.garden_id)},
+            {"$push": {"plant_ids": props["_id"]}},
         )
-        if garden:
-            garden_info = GardenOut(
-                id=str(garden["_id"]),
-                name=garden["name"],
-                location=garden["location"],
-            )
-            result = self.plants_collection.insert_one(
-                {**plant.dict(), "garden_id": garden_info.id}
-            )
-            inserted_id = str(result.inserted_id)
-            return PlantOut(
-                id=inserted_id,
-                name=plant.name,
-                garden=garden_info,
-                plant_picture=plant.plant_picture,
-            )
-        else:
-            return Error(message="Invalid Garden Name")
-
-    # def get_all(self) -> List[PlantOut]:
-    #     plants = self.plants_collection.find()
-    #     plantsPropsList = list(plants)
-    #     for plantProps in plantsPropsList:
-    #         plantProps["id"] = str(plantProps["_id"])
-    #     return [PlantOut(**plant) for plant in plantsPropsList]
+        return PlantOut(**props)
 
     def get_all(self) -> List[PlantOut]:
-        plants = []
-        for plant in self.plants_collection.find().sort("name"):
-            garden_id = plant["garden_id"]
-            garden = self.gardens_collection.find_one(
-                {"_id": ObjectId(garden_id)}
-            )
-            if garden:
-                garden_info = GardenOut(
-                    id=str(garden["_id"]),
-                    name=garden["name"],
-                    location=garden["location"],
-                )
-                plant_info = PlantOut(
-                    id=str(plant["_id"]),
-                    name=plant["name"],
-                    garden=garden_info,
-                    plant_picture=plant["plant_picture"],
-                )
-                plants.append(plant_info)
-        return plants
+        plants = self.plants_collection.find()
+        plantsPropsList = list(plants)
+        for plantProps in plantsPropsList:
+            plantProps["id"] = str(plantProps["_id"])
+        return [PlantOut(**plant) for plant in plantsPropsList]
 
-    # def get_one(self, plant_id: str) -> List[PlantOut]:
-    #     plant = self.plants_collection.find_one({"_id": ObjectId(plant_id)})
-    #     plant["id"] = str(plant["_id"])
-    #     return PlantOut(**plant)
-
-    def get_one(self, plant_id: str) -> PlantOut:
+    def get_one(self, plant_id: str) -> List[PlantOut]:
         plant = self.plants_collection.find_one({"_id": ObjectId(plant_id)})
-        if plant is None:
-            return None
-        garden_id = plant["garden_id"]
-        garden = self.gardens_collection.find_one({"_id": ObjectId(garden_id)})
-        if garden is None:
-            return None
-        garden_info = GardenOut(
-            id=str(garden["_id"]),
-            name=garden["name"],
-            location=garden["location"],
-        )
-        return PlantOut(
-            id=str(plant["_id"]),
-            name=plant["name"],
-            garden=garden_info,
-            plant_picture=plant["plant_picture"],
-        )
+        plant["id"] = str(plant["_id"])
+        return PlantOut(**plant)
 
     def delete(self, plant_id: str) -> bool:
         plant = self.plants_collection.delete_one({"_id": ObjectId(plant_id)})
